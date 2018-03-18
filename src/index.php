9aa377dc43007ec0838de42977a0fdebf254d955
Brett Boston <boston@csail.mit.edu>
2018-03-18 13:05:32 -0400

    Added some info about the page

--- src/index.php
+++ src/index.php
@@ -15,9 +15,20 @@
       foreach ($lines as $line) {
         $line = str_replace("--", "&mdash;", $line);
 ?>
-        <p><?=$line?></p>
+        <p class=quote><?=$line?></p>
 <?php
       }
 ?>
+    <hr/>
+    <h2>What is this?</h2>
+    <p>
+      This site catalogs what little information we have about Arnaldo's
+      Dungeon.
+      If you know something about The Dungeon that we don't, you can add it to
+      the virtual dungeon yourself
+      <a href="https://github.com/bboston7/arnaldos-dungeon">on github</a>
+      or email it to
+      <a href="mailto:dungeon@untitled.earth">dungeon@untitled.earth</a>.
+    </p>
   </body>
 </html>
